5f7e0a356b4ac682df6d1bc83e216ea12367ba4e
TitanSystem <enzoblrg@gmail.com>
2026-01-18 22:49:01 -0300

    creation of a short code in the requested language

---
+++ user.service.ts
@@ -0,0 +1,8 @@
+// saludo.ts
+const profesor = "Hector";
+
+function saludarProfesor(nombre: string): string {
+  return `Hola profesor ${nombre}, ¡saludos desde GitHub!`;
+}
+
+console.log(saludarProfesor(profesor));
